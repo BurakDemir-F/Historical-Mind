5c0e0d11c333056541b96144f715473711716cdf
BurakDemir-F <burakdemir1453@gmail.com>
2022-05-12 20:45:27 +0300

    showing and hiding maze room based on player position

--- Assets/Scripts/Maze/MazeGenerator.cs
+++ Assets/Scripts/Maze/MazeGenerator.cs
@@ -1,30 +1,31 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using Algorithms;
 using Test;
-using Unity.Mathematics;
 using UnityEngine;
 
 namespace Maze
 {
     public class MazeGenerator : MonoBehaviour
     {
-        [SerializeField] private Vector2Int size;
         [SerializeField] private RoomBehaviour mazeRoom;
         [SerializeField] private float roomOffset = 7f;
         
+        private Vector2Int _size = MazeInfo.size;
         private List<Cell> _cells;
 
         private void Start()
         {
-            var backTracing = new BackTracing(size.x, size.y);
+            var backTracing = new BackTracing(_size.x, _size.y);
             _cells = backTracing.GetCells();
-            
-            GenerateMaze(_cells);
+
+            StartCoroutine(GenerateMaze(_cells));
         }
 
-        private void GenerateMaze(List<Cell> cells)
+        private IEnumerator GenerateMaze(List<Cell> cells)
         {
+            var waitForSeconds = new WaitForSeconds(0.01f);
             foreach (var cell in cells)
             {
                 if (!cell.IsVisited) continue;
@@ -34,7 +35,34 @@ namespace Maze
                     Quaternion.identity);
                 newRoom.name = $"Room: {cell.Position.x} : {cell.Position.y}";
                 newRoom.UpdateRoom(cell.GetNeighborStatuses());
+                newRoom.SetRoomPosition(cell.Position);
+                MazeInfo.AddRoom(cell.Position,newRoom);
+                yield return waitForSeconds;
             }
+
+            yield return new WaitForSeconds(3f);
+
+            #region test
+
+            var player = new GameObject("Player");
+            player.AddComponent<BasicPlayer>();
+            player.tag = "Player";
+            player.AddComponent<BoxCollider>();
+            var rigidBody = player.AddComponent<Rigidbody>();
+            rigidBody.useGravity = false;
+            player.transform.position = new Vector3(0, 2f, 0);
+            
+            if (MazeInfo.TryGetRoom(new Vector2Int(0,0), out mazeRoom))
+            {
+                mazeRoom.OnTriggerEnter(player.GetComponent<BoxCollider>());    
+            }
+            
+            var cam = Camera.main;
+            cam.transform.position = new Vector3(0, 1f, -0.5f);
+            cam.transform.SetParent(player.transform);
+            cam.transform.rotation = Quaternion.Euler(Vector3.zero);
+            #endregion
+
         }
     }
 }
--- Assets/Scripts/Maze/RoomBehaviour.cs
+++ Assets/Scripts/Maze/RoomBehaviour.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,6 +9,12 @@ namespace Maze
         /*0 - up, 1 - down, 2 - right, 3 - left*/
         [SerializeField]private GameObject[] walls,doors;
         [SerializeField]private List<bool> testStatuses;
+
+        private Vector2Int _position;
+        private void OnDestroy()
+        {
+            MazeInfo.RemoveRoom(_position);
+        }
 
         public void UpdateRoom(IReadOnlyList<bool> doorOpenStatuses)
         {
@@ -23,10 +30,39 @@ namespace Maze
                 walls[i].SetActive(!doorOpenStatuses[i]);
             }
         }
+
+        public void SetRoomPosition(Vector2Int position)
+        {
+            _position = position;
+        }
         
         public void UpdateRoomTest()
         {
             UpdateRoom(testStatuses);
+        }
+
+        public void OnTriggerEnter(Collider other)
+        {
+            if (!other.CompareTag("Player")) return;
+            if(_position == MazeInfo.currentRoomPosition) return;
+            //if(MazeInfo.GetActiveRooms().Contains(this)) return;
+
+            foreach (var room in MazeInfo.GetActiveRooms())
+            {
+                room.gameObject.SetActive(false);
+            }
+            MazeInfo.RemoveAllActiveRooms();
+
+            gameObject.SetActive(true);
+            MazeInfo.currentRoomPosition = _position;
+            MazeInfo.AddActiveRoom(this);
+            
+            var rooms = MazeInfo.GetNeighborRooms(_position);
+            foreach (var room in rooms)
+            {
+                room.gameObject.SetActive(true);
+                MazeInfo.AddActiveRoom(room);
+            }
         }
     }
 }
