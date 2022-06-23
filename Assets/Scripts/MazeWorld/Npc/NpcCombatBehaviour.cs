4eae6072f8175fa593d443050b7c689b2fea1b82
BurakDemir-F <burakdemir1453@gmail.com>
2022-06-23 19:36:11 +0300

    Npc combat behaviour started.

---
+++ Assets/Scripts/MazeWorld/Npc/NpcCombatBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace MazeWorld.Npc
+{
+    public class NpcCombatBehaviour : MonoBehaviour
+    {
+        [SerializeField]private Vector3 sharpObjectPosition;
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.DrawSphere(sharpObjectPosition,1f);
+        }
+    }
+}
